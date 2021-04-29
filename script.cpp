68bad125fa593e41b8b6b1332f6983aca2222b29
Irina Paliakova <76568191+irene-paliakova@users.noreply.github.com>
2021-04-29 11:06:33 +0300

    new script

--- script.cpp
+++ script.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include <iomanip>
+//#include <conio.h>
+//#include <Windows.h>
 #include <string>
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,43 +11,43 @@
 using namespace std;
 using namespace cgicc;
 
-#define N 3
+#define N 99999
+#define N_WORD 32
+#define TOTAL_WORDS 9999
+#define SHINGLE 3
+#define TO_BE_REMOVED "чтд кг км м г с а к но и да не или либо же а что чтобы как так, и т.д., и пр., и др. " 
+#define SEPARATORS "., !:-—;?\"\'"
 #define ENG_LETTERS "AaBCcEeHKkMmOoPpTXxYy"
-#define RUS_LETTERS "ÀàÂÑñÅåÍÊêÌìÎîÐðÒÕõÓó"
-#define NEED_SYMBOLS_TO_MAKE_LOWERCASE 32
-#define TO_BE_REMOVED "÷òä êã êì ì ã ñ à ê íî è äà íå èëè ëèáî æå à ÷òî ÷òîáû êàê òàê ò ä ïð äð "
+#define RUS_LETTERS "АаВСсЕеНКкМмОоРрТХхУу"
+#define RUS_UPP_TO_LOW_CASE 32
 
-double antiPlagiarism(string text, string fragment);
-
-int calcNumberOfWordsInString(string str1);
-
-bool isDigit(char c);
+bool isSeparator(char c);
 bool isEngLetter(char c);
-bool isSeparator(char character);
-
-string symbolProcessing(string str1);
-
-void extractWordsFromStringIntoArray(string source, string *destination);
-
-int calcNumberOfMatchingShingles(string fragment[], int wordsInFragment, string text[], int wordsInText);
-
-double calcPercentageOfCoincidenceShinglesFromTotalNumber(int hitCounter, int totalWords);
+int strLen(char str[]);
+void strCat(char str1[], char str2[]);
+void strCpy(char str1[], char str2[]);
+void strCpyStrToChar(char str1[], string str2);
+int strCmp(char str1[], char str2[]);
+void emptyTextArray(char textSplitted[][N_WORD], int numberOfRows);
+void splitText(char text[], char textSplitted[][N_WORD], char wordsToBeRemoved[][N_WORD]);
+void createWordsToBeRemoved(char toBeRemoved[], char wordsToBeRemoved[][N_WORD]);
+void replaceEngLetters(char textAr[]);
+void replaceUpperCase(char textAr[]);
+bool isWordForTransfer(char word[]);
+bool isNumber(char word[]);
+bool isToBeRemoved(char word[], char wordsToBeRemoved[][N_WORD]);
+int calcTextUnits(char textSplitted[][N_WORD]);
+double calcPlagiatRate(char textSplitted[][N_WORD], char fragmentSplitted[][N_WORD]);
+double antiPlagiat(string text, string fragment);
 string getDB();
-string removeWord(string str, string toBeRemoved);
-bool wordsAreEqual(string word1, string words);
-void changeString(string &newString, string &currentWord);
-int lettersCounter(string c);
 
+int main()
+{
+    //SetConsoleCP(1251);
+    //SetConsoleOutputCP(1251);
+    Cgicc form;
+    string name;
 
-string replaceEngLetters(string str1);
-
-string toLowerCase(string str1);
-
-
-int main() {
-	Cgicc form;
-	string name;
- 
     cout << "Content-type:text/html\r\n\r\n";
     cout << "<html>\n";
     cout << "<head>\n";
@@ -54,51 +55,293 @@ int main() {
     cout << "</head>\n";
     cout << "<body>\n";
     cout << "<p>";
-	    
-    name = form("name");
     
+    name = form("name");
     if (!name.empty()) {
-    	cout << antiPlagiarism(getDB(), name) << "\n";
+    	cout << antiPlagiat(getDB(), name) << "\n";
     } else {
     	cout << "Text is not provided!\n";
     }	
     cout << "</p>\n";
     cout << "</body>\n";
     cout << "</html>\n";
-	
+
     return 0;
 }
 
-double antiPlagiarism(string text, string fragment) {
-    int hitCounter = 0, numberOfWordsInFragment = 0, numberOfWordsInText = 0;
+double antiPlagiat(string text, string fragment) {
+    char textAr[N], 
+		fragmentAr[N], 
+		toBeRemoved[] = TO_BE_REMOVED;
+    char textSplitted[TOTAL_WORDS][N_WORD], 
+		fragmentSplitted[TOTAL_WORDS][N_WORD], 
+		wordsToBeRemoved[TOTAL_WORDS][N_WORD];
+    int textUnits = 0, 
+		fragmentUnits = 0, 
+		toRemoveUnits = 0;
 
-    fragment = symbolProcessing(fragment);
-    text = symbolProcessing(text);
-    
-    fragment = replaceEngLetters(fragment);
-    text = replaceEngLetters(text);
-    
-    fragment = toLowerCase(fragment);
-    text = toLowerCase(text);
-	
-	string toBeRemoved = TO_BE_REMOVED;
-	
-	fragment = removeWord(fragment, toBeRemoved);
-    text = removeWord(text, toBeRemoved);
-	
-    numberOfWordsInFragment = calcNumberOfWordsInString(fragment);
-    numberOfWordsInText = calcNumberOfWordsInString(text);
+    strCpyStrToChar(textAr, text);
+    strCpyStrToChar(fragmentAr, fragment);
 
-    string wordsFromFragment[numberOfWordsInFragment];
-    string wordsFromText[numberOfWordsInText];
+    replaceEngLetters(textAr);
+    replaceUpperCase(textAr);
+    replaceEngLetters(fragmentAr);
+    replaceUpperCase(fragmentAr);
 
-    extractWordsFromStringIntoArray(fragment, wordsFromFragment);
-    extractWordsFromStringIntoArray(text, wordsFromText);
+    emptyTextArray(textSplitted, TOTAL_WORDS);
+    emptyTextArray(fragmentSplitted, TOTAL_WORDS);
+    emptyTextArray(wordsToBeRemoved, TOTAL_WORDS);
 
-    hitCounter = calcNumberOfMatchingShingles(wordsFromFragment, numberOfWordsInFragment, wordsFromText,
-                                              numberOfWordsInText);
+    createWordsToBeRemoved(toBeRemoved, wordsToBeRemoved);
+    splitText(textAr, textSplitted, wordsToBeRemoved);
+    splitText(fragmentAr, fragmentSplitted, wordsToBeRemoved);
 
-    return 100 - calcPercentageOfCoincidenceShinglesFromTotalNumber(hitCounter, numberOfWordsInFragment);
+    double pagiat = calcPlagiatRate(textSplitted, fragmentSplitted);
+
+    /*cout << "Text:" << endl;
+    for (int i = 0; i < calcTextUnits(textSplitted); i++)
+        cout << i + 1 << ") \"" << textSplitted[i] << "\"" << endl;
+    cout << endl<< "Fragment:" << endl;
+    for (int i = 0; i < calcTextUnits(fragmentSplitted); i++)
+        cout << i + 1 << ") \"" << fragmentSplitted[i] << "\"" << endl;*/
+
+    return pagiat;
+}
+
+bool isSeparator(char c)
+{
+    char separator[] = SEPARATORS;
+
+    for (int i = 0; separator[i] != '\0'; i++)
+        if (c == separator[i])
+            return true;
+    return false;
+}
+
+bool isEngLetter(char c)
+{
+    return ('A' <= c and c <= 'Z') or ('a' <= c and c <= 'z');
+}
+
+int strLen(char str[])
+{
+    int len = 0;
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        len++;
+    }
+
+    return len;
+}
+
+void strCat(char str1[], char str2[])
+{
+    int len1 = strLen(str1);
+    int i;
+
+    for (i = 0; str2[i] != '\0'; i++)
+    {
+        str1[len1 + i] = str2[i];
+    }
+
+    str1[len1 + i] = '\0';
+}
+
+void strCpy(char str1[], char str2[])
+{
+    int i;
+
+    for (i = 0; str2[i] != '\0'; i++)
+    {
+        str1[i] = str2[i];
+    }
+
+    str1[i] = '\0';
+}
+
+void strCpyStrToChar(char str1[], string str2)
+{
+    int i;
+
+    for (i = 0; str2[i] != '\0'; i++)
+    {
+        str1[i] = str2[i];
+    }
+
+    str1[i] = '\0';
+}
+
+int strCmp(char str1[], char str2[])
+{
+    int i = 0;
+
+    for (i = 0; ; i++)
+    {
+        if (str1[i] > str2[i])
+            return 1;
+
+        if (str1[i] < str2[i])
+            return -1;
+
+        if (str1[i] == '\0' and str2[i] == '\0')
+            return 0;
+    }
+}
+
+void emptyTextArray(char textSplitted[][N_WORD], int numberOfRows)
+{
+    for (int i = 0; i < numberOfRows; i++)
+        textSplitted[i][0] = '\0';
+}
+
+void splitText(char text[], char textSplitted[][N_WORD], char wordsToBeRemoved[][N_WORD])
+{
+    char word[N] = "";
+    int i = 0, iw = 0, iSp = 0;
+
+    for (i = 0; text[i] != '\0'; i++) {
+        if (!isSeparator(text[i])) {
+            word[iw] = text[i];
+            iw++;
+            if (isSeparator(text[i + 1]) or text[i + 1] == '\0') {
+                word[iw] = '\0';
+                iw = 0;
+                if (word[0] != '\0' and !isNumber(word) and !isToBeRemoved(word, wordsToBeRemoved)) {
+                    strCpy(textSplitted[iSp], word);
+                    iSp++;
+                }
+                word[0] = '\0'; //?
+            }
+        }
+    }
+}
+
+void createWordsToBeRemoved(char toBeRemoved[], char wordsToBeRemoved[][N_WORD])
+{
+    char word[N_WORD] = "";
+    int i = 0, iw = 0, iSp = 0;
+
+    for (i = 0; toBeRemoved[i] != '\0'; i++) {
+        if (!isSeparator(toBeRemoved[i])) {
+            word[iw] = toBeRemoved[i];
+            iw++;
+            if (isSeparator(toBeRemoved[i + 1]) or toBeRemoved[i + 1] == '\0') {
+                word[iw] = '\0';
+                iw = 0;
+                strCpy(wordsToBeRemoved[iSp], word);
+                iSp++;
+            }
+        }
+    }
+}
+
+void replaceEngLetters(char textAr[])
+{
+    char engLetters[] = ENG_LETTERS;
+    char rusLetters[] = RUS_LETTERS;
+    int i, j;
+
+    for (i = 0; textAr[i] != '\0'; i++)
+    {
+        if (isEngLetter(textAr[i]))
+        {
+            for (j = 0; engLetters[j] != '\0'; j++)
+            {
+                if (textAr[i] == engLetters[j])
+                {
+                    textAr[i] = rusLetters[j];
+                    break;
+                }
+            }
+        }
+    }
+}
+
+void replaceUpperCase(char textAr[])
+{
+    for (int i = 0; textAr[i] != '\0'; i++)
+    {
+        if ('А' <= textAr[i] and textAr[i] <= 'Я')
+        {
+            textAr[i] = textAr[i] + RUS_UPP_TO_LOW_CASE;
+        }
+    }
+}
+
+bool isWordForTransfer(char word[])
+{
+    return word[0] != '\0' and !isNumber(word);
+}
+
+bool isNumber(char word[])
+{
+    for (int i = 0; word[i] != '\0'; i++) {
+        if (word[i] <= '0' or word[i] >= '9')
+            return false;
+    }
+
+    return true;
+}
+
+bool isToBeRemoved(char word[], char wordsToBeRemoved[][N_WORD])
+{
+    int numberOfWordsToRemove = calcTextUnits(wordsToBeRemoved);
+
+    for (int i = 0; i < numberOfWordsToRemove; i++)
+        if (strCmp(word, wordsToBeRemoved[i]) == 0)
+            return true;
+
+    return false;
+}
+
+int calcTextUnits(char textSplitted[][N_WORD])
+{
+    int textUnits = 0;
+    for (textUnits = 0; textSplitted[textUnits][0] != '\0'; textUnits++) {}
+
+    return textUnits;
+}
+
+double calcPlagiatRate(char textSplitted[][N_WORD], char fragmentSplitted[][N_WORD])
+{
+    int shinglesTotal = 0, shinglesEquial = 0;
+    double plagiat;
+    bool isEquial;
+    int iFr, iText, i;
+    int textUnits = calcTextUnits(textSplitted);
+    int fragmentUnits = calcTextUnits(fragmentSplitted);
+
+    for (iFr = 0; iFr <= fragmentUnits - SHINGLE; iFr++)
+    {
+        for (iText = 0; iText <= textUnits - SHINGLE; iText++)
+        {
+            isEquial = true;
+
+            for (i = 0; i < SHINGLE; i++)
+            {
+                if (strCmp(fragmentSplitted[iFr + i], textSplitted[iText + i]) != 0)
+                {
+                    isEquial = false;
+                    break;
+                }
+            }
+
+            if (isEquial)
+            {
+                shinglesEquial++;
+                break;  
+            }
+        }
+
+        shinglesTotal++;
+    }
+
+    plagiat = (double)shinglesEquial / (double)shinglesTotal * 100;
+
+    //cout << "Total: " << shinglesTotal << endl;
+    //cout << "Eq: " << shinglesEquial << endl;
+
+    return plagiat;
 }
 
 string getDB(){
@@ -111,198 +354,4 @@ string getDB(){
     in.close();
     
     return dbText;
-}
-
-
-bool isDigit(char c) {
-    return c >= '0' and c <= '9';
-}
-
-int calcNumberOfWordsInString(string str1) {
-    int counter = 0;
-    bool flag = false;
-
-    for (int i = 0; str1[i]; ++i) {
-        if (str1[i] != ' ' and !flag) {
-            flag = true;
-        } else if (str1[i] == ' ' and flag or str1[i + 1] == '\0' and flag) {
-            flag = false;
-            counter++;
-        }
-    }
-    return counter;
-}
-
-bool isSeparator(char character) {
-    char separators[] = " .,-:;!?()+=/*";
-
-    for (int i = 0; separators[i]; ++i) {
-        if (character == separators[i]) {
-            return true;
-        }
-    }
-    return false;
-}
-
-string symbolProcessing(string str1) {
-    for (int i = 0; str1[i]; ++i) {
-        if (isDigit(str1[i]) or isSeparator(str1[i])) {
-            str1[i] = ' ';
-        } else {}
-    }
-    return str1;
-}
-
-string replaceEngLetters(string str1)
-{
-    string engLetters = ENG_LETTERS;
-    string rusLetters = RUS_LETTERS;
-    int i, j;    
-	string s = "";
-	
-    for (i = 0; str1[i]; i++) {
-        if (isEngLetter(str1[i])) {
-            for (j = 0; str1[j]; j++) {
-                if (str1[i] == engLetters[j]) {
-                    s += rusLetters[j];
-                }
-            }
-        }
-        else 
-        	s += str1[i];
-    }
-    return s;
-}
-
-bool isEngLetter(char c)
-{
-    return ('A' <= c and c <= 'Z') or ('a' <= c and c <= 'z');
-}
-
-string toLowerCase(string str1) {
-
-    for(int i = 0; str1[i]; i++) {
-        if(str1[i] >= 'À' and str1[i] <= 'ß') {
-            str1[i] += NEED_SYMBOLS_TO_MAKE_LOWERCASE; 
-        }
-        if(str1[i] == '¨') {
-            str1[i] = '¸';
-        }
-    }
-    return str1;  
-}
-
-
-void extractWordsFromStringIntoArray(string source, string *destination) {
-    int wordCounter = 0;
-
-    for (int i = 0; source[i]; i++) {
-        if (!isSeparator(source[i]) and isSeparator(source[i + 1])) {
-            destination[wordCounter] += source[i];
-            wordCounter++;
-        } else if (!isSeparator(source[i]) and !isSeparator(source[i + 1])) {
-            destination[wordCounter] += source[i];
-        } else {}
-    }
-
-}
-
-int calcNumberOfMatchingShingles(string fragment[], int wordsInFragment, string text[], int wordsInText) {
-    int counter = 0, numberOfMatchedShingles = 0;
-
-    for (int i = 0; i < wordsInFragment - N + 1; ++i) {
-        for (int j = 0; j < wordsInText - N + 1; ++j) {
-            for (int k = 0; k < N; ++k) {
-                if (fragment[i + k] == text[j + k]) {
-                    numberOfMatchedShingles++;
-                }
-            }
-            if (numberOfMatchedShingles == N) {
-                counter++;
-            }
-            numberOfMatchedShingles = 0;
-        }
-    }
-    return counter;
-}
-
-double calcPercentageOfCoincidenceShinglesFromTotalNumber(int hitCounter, int totalWords) {
-    int totalNumberOfShingles = totalWords - N + 1;
-    return ((double) hitCounter / totalNumberOfShingles) * 100;
-}
-
-string removeWord(string str1, string toBeRemoved)
-{
-	string currentWord = "";	
-	string newString = "";
-	
-	for(int i = 0; str1[i] ; i++) {
-		if(str1[i] >= 'à' and str1[i] <= 'ÿ'){
-			currentWord += str1[i];
-		} 
-		if(str1[i]== ' '){	
-			if(!wordsAreEqual(currentWord, toBeRemoved)) {
-				changeString(newString, currentWord);
-			}
-			if(wordsAreEqual(currentWord, toBeRemoved))
-				currentWord = "";	
-		}
-	}
-	
-	if(!wordsAreEqual(currentWord, toBeRemoved)){
-		newString += ' ' + currentWord;
-	}
-	
-	return newString;
-}
-
-bool wordsAreEqual(string word1, string words) {	
-	int counterWord1 = lettersCounter(word1),
-		counterWords = 0;
-	string currentWord = "";
-	
-	bool areEqual = false; 	
-	
-	for(int i = 0; words[i]; i++) {
-		if(words[i] != ' ') {
-			counterWords++;
-			currentWord += words[i];
-		}
-		else if(words[i] == ' ' and counterWord1 == counterWords) {
-			if(currentWord == word1) {
-				areEqual = true;
-			}
-			currentWord = "";
-			counterWords = 0;
-		}
-		else if(words[i] == ' ' and counterWord1 != counterWords) {
-			currentWord = "";
-			counterWords = 0;
-		}
-		else{}			
-	}
-	return areEqual;
-}
-
-int lettersCounter(string c)
-{	
-	int counter = 0;
-	
-	for(int i = 0; c[i]; i++){
-		counter++;
-	}
-	
-	return counter;
-}
-
-void changeString(string &newString, string &currentWord)
-{
-	if(newString == ""){
-		newString += currentWord;
-		currentWord = "";
-	}					
-	else {
-		newString += ' ' + currentWord;
-		currentWord = "";
-	}
 }
